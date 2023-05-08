9879271d5e37f7aed5b0f80d1521f87e212c622a
Safdar Thaheem <safdarthaheem716@gmail.com>
2023-05-08 11:07:06 -0700

    template banner implementation

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+const hamMenu = document.getElementById('hamburger-menu');
+const offScreenMenu = document.querySelector('.off-screen');
+hamMenu.addEventListener('click', () => {
+	hamMenu.classList.toggle('active');
+	offScreenMenu.classList.toggle('active');
+});
+
+/* Adding class to navigation bar on scroll */
+$(window).scroll(function() {
+    var nav = $('#main-nav');
+    var top = 50;
+    if ($(window).scrollTop() >= top) {
+
+        nav.addClass('illuminated-nav');
+
+    } else {
+        nav.removeClass('illuminated-nav');
+    }
+});

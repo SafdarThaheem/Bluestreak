2d0d5296638f2bb6a2ec25945a2ae0fde825932f
Safdar Thaheem <safdarthaheem716@gmail.com>
2023-05-17 11:29:10 -0700

    menu designing and changing the tabs section

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,6 +18,42 @@ $(window).scroll(function() {
     }
 });
 
+// accordion to tabs
+$(document).ready(function() {
+    //----------Select the first tab and div by default
+    $('#vertical_tab_nav > ul > li > a').eq(0).addClass("selected");
+    $('#vertical_tab_nav > div > article').eq(0).css('display', 'block');
+    //---------- This assigns an onclick event to each tab link("a" tag) and passes a parameter to the showHideTab() function
+    $('#vertical_tab_nav > ul').click(function(e) {
+      if ($(e.target).is("a")) {
+        /*Handle Tab Nav*/
+        $('#vertical_tab_nav > ul > li > a').removeClass("selected");
+        $(e.target).addClass("selected");
+        /*Handles Tab Content*/
+        var clicked_index = $("a", this).index(e.target);
+        $('#vertical_tab_nav > div > article').css('display', 'none');
+        $('#vertical_tab_nav > div > article').eq(clicked_index).fadeIn();
+      }
+      $(this).blur();
+      return false;
+    });
+  }); //end ready
+      
+  /* if in drawer mode */
+  $(".tab_drawer_heading").click(function() {
+  
+    $("article").hide();
+    var d_activeTab = $(this).attr("rel"); 
+    $("#"+d_activeTab).fadeIn();
+  
+    $(".tab_drawer_heading").removeClass("d_active");
+    $(this).addClass("d_active");
+  
+    $("ul.tabs li a").removeClass("selected");
+    $("ul.tabs li a[rel^='"+d_activeTab+"']").addClass("selected");
+  });
+// end
+
 // awards section slider
 $('.owl-carousel').owlCarousel({
     loop:true,
@@ -25,7 +61,7 @@ $('.owl-carousel').owlCarousel({
     nav:false,
     dots:false,
     autoplay:true,
-    autoplayTimeout:3000,
+    autoplayTimeout:30000,
     responsive:{
         0:{
             items:1
